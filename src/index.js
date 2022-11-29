ddfc7a12bd6b3d72fd1d7c77ab6b23a82af643a2
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2022-11-29 12:25:46 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
